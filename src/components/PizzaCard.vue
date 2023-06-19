84ff829f50d1fff7a8a59cb879ed322a6d634d40
silviosalza <silviofilipposalza@gmail.com>
2023-06-19 16:52:39 +0200

    bonus

---
+++ src/components/PizzaCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    name: "PizzaCard",
+    props:{
+        pizza: Object,
+    },
+    computed:{
+        
+    }
+}
+</script>
+
+<template>
+    <div class="card text-center">
+        <h4>{{ pizza.name }}</h4>
+        <p>{{ pizza.price }}</p>
+        <p>{{ pizza.description }}</p>
+        <span v-for="(ingredient,index) in pizza.ingredients">
+        <h6>{{ ingredient.name + " " }}</h6></span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.card{
+    height: 180px;
+    margin-top: 2rem;
+    background-color: rgb(88, 88, 101);
+    color: white;
+}
+</style>
---
+++ src/pages/PizzasList.vue
@@ -0,0 +1,48 @@
+<script>
+import axios from 'axios';
+import PizzaCard from '../components/PizzaCard.vue'
+
+export default{
+    name:'PizzasList',
+    components: {
+    PizzaCard,
+  },
+  data(){
+    return{
+      pizzas:[]
+    }
+  },
+  mounted(){
+    this.getPizza();
+  },
+  methods: {
+    getPizza(){
+      axios.get('http://127.0.0.1:8000/api/pizzas').then(resp => {
+        this.pizzas = resp.data.results;
+
+      })
+
+
+    }
+  },
+
+}
+
+</script>
+
+
+
+
+<template>
+    <div class="row row-cols-4">
+        <div class="col" v-for="(pizza,index) in pizzas" :key="index">
+            <PizzaCard :pizza="pizza"/>
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
